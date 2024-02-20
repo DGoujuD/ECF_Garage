70f41efe8adedb166fcae6c1027f3d486651e7cf
DGoujuD <116717480+DGoujuD@users.noreply.github.com>
2024-02-20 21:25:05 +0100

    files Back-end of the project

---
+++ Back-end/garage.sql
@@ -0,0 +1,234 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 20 fév. 2024 à 21:20
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `garage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avis`
+--
+
+CREATE TABLE `avis` (
+  `id_avis` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `commentaire` varchar(1000) NOT NULL,
+  `note` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id_client` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `num_tel` int(50) DEFAULT NULL,
+  `prestation` varchar(100) DEFAULT NULL,
+  `somme_payee` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id_message` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `num_tel` varchar(50) DEFAULT NULL,
+  `message` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employes`
+--
+
+CREATE TABLE `employes` (
+  `id_employe` int(11) NOT NULL,
+  `mot_de_passe` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `date_de_naissance` date DEFAULT NULL,
+  `tel_pro` varchar(12) NOT NULL,
+  `mail_pro` varchar(50) NOT NULL,
+  `poste` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `employes`
+--
+
+INSERT INTO `employes` (`id_employe`, `mot_de_passe`, `nom`, `prenom`, `date_de_naissance`, `tel_pro`, `mail_pro`, `poste`) VALUES
+(1, '**********', 'Parrot', 'Vincent', '1986-11-09', '0601020304', 'Parrot.Vince@garage.vroum', 'Administrateur');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `horaire`
+--
+
+CREATE TABLE `horaire` (
+  `id_jour` int(11) NOT NULL,
+  `nom_jour` varchar(8) NOT NULL,
+  `heure_ouverture_matin` varchar(5) DEFAULT NULL,
+  `heure_fermeture_matin` varchar(5) DEFAULT NULL,
+  `heure_ouverture_soir` varchar(5) DEFAULT NULL,
+  `heure_fermeture_soir` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `horaire`
+--
+
+INSERT INTO `horaire` (`id_jour`, `nom_jour`, `heure_ouverture_matin`, `heure_fermeture_matin`, `heure_ouverture_soir`, `heure_fermeture_soir`) VALUES
+(1, 'Lundi', '08h45', '12h00', '14h00', '18h00'),
+(2, 'Mardi', '08h45', '12h00', '14h00', '18h00'),
+(3, 'Mercredi', '08h45', '12h00', '14h00', '18h00'),
+(4, 'Jeudi', '08h45', '12h00', '14h00', '18h00'),
+(5, 'Vendredi', '08h45', '12h00', '14h00', '18h00'),
+(6, 'Samedi', '08h45', '12h00', '', ''),
+(7, 'Dimanche', 'Fermé', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image_vehicule`
+--
+
+CREATE TABLE `image_vehicule` (
+  `id_vehicule` varchar(50) NOT NULL,
+  `adresse_img` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `services`
+--
+
+CREATE TABLE `services` (
+  `id_prestation` varchar(50) NOT NULL,
+  `prix` float NOT NULL,
+  `description` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vehicules`
+--
+
+CREATE TABLE `vehicules` (
+  `id_vehicule` varchar(20) NOT NULL,
+  `marque` varchar(20) NOT NULL,
+  `modele` varchar(20) NOT NULL,
+  `version` varchar(30) NOT NULL,
+  `annee` int(11) NOT NULL,
+  `kilometrage` int(11) NOT NULL,
+  `puissance_fiscale` int(11) NOT NULL,
+  `energie` varchar(10) NOT NULL,
+  `boîte_de_vitesse` varchar(12) NOT NULL,
+  `commentaire` varchar(1000) NOT NULL,
+  `image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD PRIMARY KEY (`id_avis`);
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id_client`);
+
+--
+-- Index pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id_message`);
+
+--
+-- Index pour la table `employes`
+--
+ALTER TABLE `employes`
+  ADD PRIMARY KEY (`id_employe`);
+
+--
+-- Index pour la table `horaire`
+--
+ALTER TABLE `horaire`
+  ADD PRIMARY KEY (`id_jour`);
+
+--
+-- Index pour la table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id_prestation`);
+
+--
+-- Index pour la table `vehicules`
+--
+ALTER TABLE `vehicules`
+  ADD PRIMARY KEY (`id_vehicule`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `avis`
+--
+ALTER TABLE `avis`
+  MODIFY `id_avis` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `employes`
+--
+ALTER TABLE `employes`
+  MODIFY `id_employe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
